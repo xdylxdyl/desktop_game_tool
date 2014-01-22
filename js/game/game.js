fd1ba9e65a96370ed85e8d055fced60b11fc8523
xdylxdyl <xdylxdyl@163.com>
2014-01-22 20:19:51 +0800

    fix bug of safari

--- js/game/game.js
+++ js/game/game.js
@@ -5,16 +5,17 @@
  * Time: 上午11:26
  * To change this template use File | Settings | File Templates.
  */
-function gameModelList($scope){
-    $scope.modelList=[
+function gameModelList($scope) {
+    $scope.test_title = "aaa";
+    $scope.officialList = [
         {
             "title":"捉鬼游戏",
-            "versions":
-            [
+            "id":"ghost",
+            "versions":[
                 {
                     "name":"简化版(1.0)",
                     "ver":1
-                 },
+                },
                 {
                     "name":"猜词版(2.0)",
                     "ver":2
@@ -28,47 +29,89 @@ function gameModelList($scope){
         },
         {
             "title":"杀人游戏",
-            "versions":
-                [
-                    {
-                        "name":"简化版",
-                        "ver":4
-                    },
-                    {
-                        "name":"警版",
-                        "ver":5
-                    },
-                    {
-                        "name":"不翻牌",
-                        "ver":6
-                    },
-                    {
-                        "name":"3.0",
-                        "ver":7
-                    }
+            "id":"kill",
+            "versions":[
+                {
+                    "name":"简化版",
+                    "ver":4
+                },
+                {
+                    "name":"警版",
+                    "ver":5
+                },
+                {
+                    "name":"不翻牌",
+                    "ver":6
+                },
+                {
+                    "name":"3.0",
+                    "ver":7
+                }
 
-                ]
+            ]
         },
         {
             "title":"狼人杀",
-            "versions":
-                [
-                    {
-                        "name":"狼人杀",
-                        "ver":8
-                    }
-                ]
+            "id":"wolf",
+            "versions":[
+                {
+                    "name":"狼人杀",
+                    "ver":8
+                }
+            ]
         },
         {
             "title":"炸狼堡",
-            "versions":
-                [
-                    {
-                        "name":"炸狼堡",
-                        "ver":9
-                    }
-                ]
+            "versions":[
+                {
+                    "name":"炸狼堡",
+                    "ver":9
+                }
+            ]
         }
+
+    ]
+    $scope.userList = [
+        {
+            "title":"三方",
+            "id":"1",
+            "versions":[
+                {
+                    "name":"简化变异小三方",
+                    "ver":1
+                }
+
+
+            ]
+        },
+        {
+            "title":"最爱",
+            "id":"2",
+            "versions":[
+                {
+                    "name":"三十六人象棋杀",
+                    "ver":4
+                },
+                {
+                    "name":"天使与魔鬼",
+                    "ver":4
+                },
+                {
+                    "name":"捉鬼变异版",
+                    "ver":4
+                },
+                {
+                    "name":"狼人杀新月扩展",
+                    "ver":4
+                },
+                {
+                    "name":"炸狼堡全角色",
+                    "ver":4
+                }
+
+            ]
+        }
+
 
     ]
 }
