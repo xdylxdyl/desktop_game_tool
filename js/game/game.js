0d5607aafbea669467fecb02c308d770668decd0
xdylxdyl <xdylxdyl@163.com>
2014-02-20 13:41:01 +0800

    fix bug of html storage

--- js/game/game.js
+++ js/game/game.js
@@ -30,46 +30,46 @@ var app = angular.module('gameTool', [], function ($compileProvider) {
 
 });
 
-app.filter('convent',function(){
+app.filter('convent', function () {
     console.log("test convent");
     var gameid = getParameterFromUrl(location.href, "gameid");
-   var  CN=dataService.getConfigCN(gameid);//获取汉字列表
-    return function(input){
-        if(input in CN)
+    var CN = dataService.getConfigCN(gameid);//获取汉字列表
+    return function (input) {
+        if (input in CN)
             return CN[input];
         else
             return input;
     }
 });
 
-app.filter('hint',function(){
-   var ROLES=dataService.getGameDetail().role.obj;//获取细节
-   var HINTCONFIG=gameService.getHintConfig();
-    return function(id){
-        var role=ROLES[id];
-        if(role==undefined){
+app.filter('hint', function () {
+    var ROLES = dataService.getGameDetail().role.obj;//获取细节
+    var HINTCONFIG = gameService.getHintConfig();
+    return function (id) {
+        var role = ROLES[id];
+        if (role == undefined) {
             return "";
         }
         return HINTCONFIG[role];
     }
 });
-app.filter('showCu',function(){
-    var gameDetail=dataService.getGameDetail();
-    var roles=gameDetail.role.obj;
-    var properties=gameDetail.properties;
-    return function(pro,id){
-            switch (pro){
-                case 'role':
-                     return roles[id];
-                default :
-                     return properties[roles[id]][pro];
-            }
+app.filter('showCu', function () {
+    var gameDetail = dataService.getGameDetail();
+    var roles = gameDetail.role.obj;
+    var properties = gameDetail.properties;
+    return function (pro, id) {
+        switch (pro) {
+            case 'role':
+                return roles[id];
+            default :
+                return properties[roles[id]][pro];
+        }
     }
 });
-app.filter('showJu',function(){
-    var gameDetail=dataService.getGameDetail();
-    var properties=gameDetail.properties;
-    return function(role,para){
+app.filter('showJu', function () {
+    var gameDetail = dataService.getGameDetail();
+    var properties = gameDetail.properties;
+    return function (role, para) {
         switch (para) {
             case 'role':
                 return properties[role]['num'];
@@ -78,93 +78,99 @@ app.filter('showJu',function(){
         }
     }
 });
-app.filter('key',function(){
-   return function(obj){
-            for(var i in obj)
-                return i;
-   }
+app.filter('key', function () {
+    return function (obj) {
+        for (var i in obj)
+            return i;
+    }
 });
-app.filter('showInit',function(){
-    var gameConfig=gameService.getGameConfig();
-    var playerNumList=gameService.getGamePlayerList(gameConfig);
-    return function(role,playerNum){
-      if(playerNum in playerNumList)
-        return playerNumList[playerNum][role];
-      else
-        return playerNumList["default"][role];
-   }
+app.filter('showInit', function () {
+    var gameConfig = gameService.getGameConfig();
+    var playerNumList = gameService.getGamePlayerList(gameConfig);
+    return function (role, playerNum) {
+        if (playerNum in playerNumList)
+            return playerNumList[playerNum][role];
+        else
+            return playerNumList["default"][role];
+    }
 });
-app.controller("gameModelList",function($scope) {
+app.controller("gameModelList", function ($scope) {
 
     $scope.officialList = dataService.getGameList(constants.listType.official);
     $scope.userList = dataService.getGameList(constants.listType.user);
 
 });
 
-app.controller("gameInitCtrl",function($scope) {
+app.controller("gameInitCtrl", function ($scope) {
 
-   /***** init  ******/
-        var gameConfig={},
-        gc=gameService.getGameConfig();
-        $scope.gameConfig=gameService.gameConfigMaker(gc,'init');
-        $scope.peopleNum =gc.playerNumDefault;//init select
-        $scope.gameInit = function(){
-            /*****  initVar => buildPropertiesList => roleMaker => setGameDetail ***/
-           var formData=document.getElementsByTagName('input');
-           var roleData=gameService.buildRoleMakeData(formData,gameConfig.playerNum);
-           var propertiesData=gameService.buildProperties(formData,gc);
-            dataService.setGameDetail(gameService.roleMaker(roleData),propertiesData);
+    /***** init  ******/
+    var gameConfig = {},
+        gc = gameService.getGameConfig();
+    $scope.gameConfig = gameService.gameConfigMaker(gc, 'init');
+    $scope.peopleNum = gc.playerNumDefault;//init select
+    $scope.gameInit = function () {
+        /*****  initVar => buildPropertiesList => roleMaker => setGameDetail ***/
+
+        try {
+
+            var formData = document.getElementsByTagName('input');
+            var roleData = gameService.buildRoleMakeData(formData, gameConfig.playerNum);
+            var propertiesData = gameService.buildProperties(formData, gc);
+            dataService.setGameDetail(gameService.roleMaker(roleData), propertiesData);
+        } catch (e) {
+            alert(" start game error " + e);
+
         }
-        $scope.gameExit = function(){
-            dataService.deleteGameDetail();
-        }
+    }
+    $scope.gameExit = function () {
+        dataService.deleteGameDetail();
+    }
 })
 
-app.controller("gamePlayCtrl",function($scope) {
+app.controller("gamePlayCtrl", function ($scope) {
     var gameid = getParameterFromUrl(location.href, "gameid"),
-        gc=dataService.getConfig(gameid);
+        gc = dataService.getConfig(gameid);
 
 
     /********** scope properties bind *************/
-    $scope.gameConfig=gameService.gameConfigMaker(gc,'play');
-    $scope.flag=false;
-    $scope.currentId=1;
-    $scope.showCurrentGamer=function(){
-        $scope.flag=true;
+    $scope.gameConfig = gameService.gameConfigMaker(gc, 'play');
+    $scope.flag = false;
+    $scope.currentId = 1;
+    $scope.showCurrentGamer = function () {
+        $scope.flag = true;
     }
-    $scope.currentGamerEraser=function(id){
-        $scope.currentId=id+1;
-        if(id >= $scope.gameConfig.playerNum) return;
-        $scope.flag=false;
+    $scope.currentGamerEraser = function (id) {
+        $scope.currentId = id + 1;
+        if (id >= $scope.gameConfig.playerNum) return;
+        $scope.flag = false;
     }
-    $scope.gameExit = function(){
+    $scope.gameExit = function () {
         dataService.deleteGameDetail();
     }
 });
 
-app.controller("judgeScanCtrl",function($scope) {
+app.controller("judgeScanCtrl", function ($scope) {
     var gameid = getParameterFromUrl(location.href, "gameid");
-    var gameConfig=dataService.getConfig(gameid);
-    $scope.gameConfig = gameService.gameConfigMaker(gameConfig,'play');
-    $scope.gameExit = function(){
+    var gameConfig = dataService.getConfig(gameid);
+    $scope.gameConfig = gameService.gameConfigMaker(gameConfig, 'play');
+    $scope.gameExit = function () {
         dataService.deleteGameDetail();
     }
 });
 
-app.controller("gameTestCtrl",function($scope){
+app.controller("gameTestCtrl", function ($scope) {
 
 });
 
 
-var introduceApp= angular.module('introduce', ["ngSanitize"], function ($compileProvider) {
-
+var introduceApp = angular.module('introduce', ["ngSanitize"], function ($compileProvider) {
 
 
 });
 
-introduceApp.controller("introduceGameCtrl",function($scope) {
+introduceApp.controller("introduceGameCtrl", function ($scope) {
 
-    $scope.gameConfig=gameService.getGameConfig($("#gid").val());
+    $scope.gameConfig = gameService.getGameConfig($("#gid").val());
 
 
 });
